0f7b08ddfd5df0f9f3db23aba0885c221b20f153
JonjimenezF <jon.jimenez@duocuc.cl>
2024-05-14 23:12:13 -0400

    subiendo repositorio

---
+++ InterEns PyStore/main.py
@@ -0,0 +1,91 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import os
+import uuid 
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+URL_SUPEBASE = 'https://gglsaoykhjniypthjgfc.supabase.co/rest/v1/'
+supebaseheads = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnbHNhb3lraGpuaXlwdGhqZ2ZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ1NTIwMTQsImV4cCI6MjAzMDEyODAxNH0.jmngoEfB87raLwTHDq1DI347a4owyHCqs75VSJUwMZo'
+
+@app.route('/agregar_producto', methods=['POST'])
+def agregar_producto():
+    producto = request.json
+    headers = {'apikey': supebaseheads}
+    # Convertimos el objeto producto a JSON antes de pasarlo a requests.post
+    response = requests.post(URL_SUPEBASE + 'PRODUCTO?', json=producto, headers=headers)
+    
+     # Verificamos si la respuesta del servidor es válida
+    if response.status_code == 200:
+        try:
+            # Intentamos obtener el JSON de la respuesta
+            data = response.json()
+        except json.decoder.JSONDecodeError:
+            # Si hay un error al decodificar el JSON, devolvemos un mensaje de error genérico
+            return jsonify({'error': 'Error al procesar la respuesta del servidor'}), 500
+
+        # Verificamos si la solicitud fue exitosa
+        if response.ok:
+            return jsonify(data), response.status_code
+        else:
+            # Si la solicitud no fue exitosa, devolvemos los detalles del error
+            return jsonify({'error': data}), response.status_code
+    else:
+        # Si la respuesta del servidor no es válida, devolvemos un mensaje de error
+        return jsonify({'error': 'Error en la respuesta del servidor'}), response.status_code
+
+# Obtener todos los productos de Supabase
+@app.route('/obtener_productos', methods=['GET'])
+def obtener_productos():
+    headers = {'apikey': supebaseheads}
+    response = requests.get(URL_SUPEBASE + 'PRODUCTO?select=*', headers=headers)
+    return response.json(), response.status_code
+
+app.config['UPLOAD_FOLDER'] = 'upload'  # Carpeta donde se almacenan las imágenes subidas
+# Lista de extensiones permitidas
+extensiones_permitidas = {'jpg', 'jpeg', 'png', 'gif'}
+
+# Subir foto
+@app.route('/upload', methods=['POST'])
+def subir_foto():
+    if 'foto' not in request.files:
+        return jsonify({'error': 'No se encontró ninguna foto en la solicitud'}), 400
+    
+    foto = request.files['foto']
+    if foto.filename == '':
+        return jsonify({'error': 'Nombre de archivo no válido'}), 400
+    
+    if '.' not in foto.filename or foto.filename.rsplit('.', 1)[1].lower() not in extensiones_permitidas:
+        return jsonify({'error': 'Extensión de archivo no permitida'}), 400
+    
+    nombre_unido = str(uuid.uuid4()) + '.' + foto.filename.rsplit('.', 1)[1].lower()
+    if not os.path.exists('upload'):
+        os.makedirs('upload')
+    
+    foto.save(os.path.join('upload', nombre_unido))    
+    return jsonify({'mensaje': 'Foto subida correctamente', 'nombre_foto': nombre_unido}), 200
+
+# Ruta para ver una foto
+@app.route('/foto/<nombre_foto>', methods=['GET'])
+def ver_foto(nombre_foto):
+    ruta_foto = os.path.join('upload', nombre_foto)
+    if not os.path.exists(ruta_foto):
+        return jsonify({'error': 'Foto no encontrada'}), 404
+    
+    return send_file(ruta_foto, mimetype='image/jpeg')
+
+# Ruta para eliminar una foto
+@app.route('/eliminar_foto/<nombre_foto>', methods=['DELETE'])
+def eliminar_foto(nombre_foto):
+    ruta_foto = os.path.join('upload', nombre_foto)
+    if not os.path.exists(ruta_foto):
+        return jsonify({'error': 'Foto no encontrada'}), 404
+
+    os.remove(ruta_foto)  # Eliminar la foto del sistema de archivos
+    return jsonify({'mensaje': 'Foto eliminada correctamente'}), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ InterEns PyStore/reame.md
@@ -0,0 +1,19 @@
+
+[] python3 o python -m venv env {crea ambiente virtual}
+    [] pip install -r requirements.txt
+
+Contenido
+[] Api minima con flask
+    []Crear carpeta
+    []Subir foto
+        []restringir extension
+    []confirmar subida
+    [] Ver fotos
+
+[]Desde angular tienes que enviar un formulario con la foto y esperar la respuesta , la respuesta la debes guardar en tu supabase
+[]luego desde angualar tienes que enviar una peticion get a la api para recupar la foto.
+[] video de ejemplo funcionamiento
+[] Instalacion + arranque
+
+
+
